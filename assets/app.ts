fc926fdfa8fe92d569b9112c9badcfab71409243
amiceli <antoine@miceli.click>
2026-06-24 22:46:30 +0200

    feat(icons): serve nordhealth icons locally

--- assets/app.ts
+++ assets/app.ts
@@ -1,7 +1,18 @@
 import './styles/app.css'
+import { Icon } from '@nordhealth/components'
 import '@nordhealth/components'
 import { registerControllers, startStimulusApp } from 'vite-plugin-symfony/stimulus/helpers'
 import { registerVueControllerComponents, type VueModule } from 'vite-plugin-symfony/stimulus/helpers/vue'
+
+Icon.registerResolver((name) =>
+    fetch(`/images/icons/${name}.svg`).then((res) => {
+        if (!res.ok) {
+            throw new TypeError(`NORD: unknown icon: '${name}'`)
+        }
+
+        return res.text()
+    }),
+)
 
 registerVueControllerComponents(import.meta.glob<VueModule>('./vue/controllers/**/*.vue'))
 
